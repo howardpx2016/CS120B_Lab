e1013cd1c77a850eb7774c3b44a3dba883a29dac
howardpx2016 <cchen277@wch136-22.cs.ucr.edu>
2020-04-15 21:38:40 -0700

    Lab 2 part 1

---
+++ cchen277_lab2_part1.c
@@ -0,0 +1,37 @@
+/*	Author: cchen277
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRB = 0xFF; PORTB = 0x00;
+    	unsigned char tmpA = 0x00;
+    /* Insert your solution below */
+    while (1) {
+	tmpA = PINA & 0x03;
+	if (tmpA == 0x00) {
+		PORTB = 0x00;
+	}
+	else if (tmpA == 0x01) {
+		PORTB = 0X01;
+	}
+	else if (tmpA == 0x02) {
+		PORTB = 0x00;
+	}
+	else if (tmpA == 0x03) {
+		PORTB = 0x00;
+	}
+    }
+    return 1;
+}
